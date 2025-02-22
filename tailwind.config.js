ca604a2d117da313e3efed811aa2602bab99bc84
geeshanikaveesha <it22605320@my.sliit.lk>
2025-02-22 14:42:49 +0530

    final commit4

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,22 @@
 /** @type {import('tailwindcss').Config} */
-const colors = require("tailwindcss/colors");
+import colors, { yellow, pink } from "tailwindcss/colors";
 
-module.exports = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+export const content = [
+  "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+  "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+  "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+];
+export const theme = {
+  extend: {
+    colors: {
+      background: "var(--background)",
+      foreground: "var(--foreground)",
     },
-    colors: 
-    {
-      ...colors,
-      primary: colors.yellow,
-  secondary:colors.pink,
-},      
-    
   },
-  plugins: [],
+  colors: {
+    ...colors,
+    primary: yellow,
+    secondary: pink,
+  },
 };
+export const plugins = [];
