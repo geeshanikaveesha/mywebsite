8610dc109eb7f5d437e737ddbc56d1a7c51f34b8
Kaveesha Erandi <156656481+geeshanikaveesha@users.noreply.github.com>
2025-04-29 14:22:58 +0530

    Update NavBar.jsx

--- src/app/components/NavBar.jsx
+++ src/app/components/NavBar.jsx
@@ -24,7 +24,7 @@ const NavBar = () => {
         <nav className="fixed  x=auoto border border-[#33353F] top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-100">
             <div className="flex container lg:py-4 flex-wrap items-center justify-between mx-auto px-4 py-2">
                 <Link href={"/"}
-                    className="text-2xl md:text-5xl text-white font-semibold">
+                    className="text-2xl md:text-5xl font-semibold">
                     👨🏼‍💻
                 </Link>
                 <div className=" mobile-menu block md:hidden">
