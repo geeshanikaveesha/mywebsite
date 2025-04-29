51720cb822f40d00fccb4000b53bc92270c1f83b
Kaveesha Erandi <156656481+geeshanikaveesha@users.noreply.github.com>
2025-04-29 14:21:53 +0530

    Update HeroSection.jsx

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -13,8 +13,7 @@ const HeroSection = () => {
                     initial={{ opacity: 0 , scale:0.5}}
                     animate={{ opacity: 1 ,scale:1}}
                     transition={{ duration: 0.5 }}
-                    className="col-span-8 place-self-center text-center sm:text-left justify-self-start"
-                >
+                    className="col-span-8 place-self-center text-center sm:text-left justify-self-start" >
                     <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg-leading-normal font-extrabold">
                         <span className="text-transparent bg-clip-text bg-gradient-to-r from-primary-400 to-secondary-600">
                             Hello,I&apos;m {" "}
